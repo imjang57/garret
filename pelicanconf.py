002f08287198758adfc0b733cf62603c86027602
imjang57 <imjang57@gmail.com>
2016-12-29 23:16:09 -0500

    Modify pellicanconf.py

--- pelicanconf.py
+++ pelicanconf.py
@@ -2,12 +2,34 @@
 # -*- coding: utf-8 -*- #
 from __future__ import unicode_literals
 
+################################################################################
+# Site information
 AUTHOR = 'imjang57'
 SITENAME = 'garret'
 SITEURL = ''
 
+################################################################################
+# Paths
 PATH = 'content'
+# Relative to PATH
+OUTPUT_PATH = 'output/'
+# Relative to PATH
+PAGE_PATHS = ['pages']
+# Relative to PATH
+STATIC_PATHS = ['images']
 
+################################################################################
+# Site generation options
+USE_FOLDER_AS_CATEGORY = True
+DEFAULT_CATEGORY = 'misc'
+DEFAULT_DATE_FORMAT = '%a %d %B %Y'
+DISPLAY_PAGES_ON_MENU = True
+DISPLAY_CATEGORIES_ON_MENU = True
+# title => slugify from Title: metadata
+# basename => slugify from file name
+SLUGIFY_SOURCE = 'title'
+
+################################################################################
 TIMEZONE = 'Asia/Seoul'
 
 DEFAULT_LANG = 'en'
